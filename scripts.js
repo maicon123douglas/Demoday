2d6e3fd0ff28cb22114891cf387c9ab5a4f2431c
Carlos Ferreira <juniorcaf30@gmail.com>
2018-05-07 03:55:53 -0300

    site 1.0

--- scripts.js
+++ scripts.js
@@ -3,19 +3,22 @@ let botao1 = document.querySelector("#botao1");
 let botao2 = document.querySelector("#botao2");
 let option = document.querySelector("header span");
 let sidebar = document.querySelector(".sidebar");
+let mais = document.querySelector(".details1");
 
+ 
 botao1.onclick = function(){
-    console.log("pg1");
     paginas[1].style.transform = "translateX(0)";
 }
 
 botao2.onclick = function(){
-    console.log("pg2");
-    paginas[2].style.transform = "translateX(0)";
+    paginas[1].style.transform = "translateX(-100%)";
 }
 
 option.onpointerenter = function(){
     sidebar.style.display= "block";
+    mais.onclick = function(){
+        paginas[2].style.transform="translateX(0)";
+    }
     
  }   
 
